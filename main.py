fed7c4c9e6f48e2c22783409db7aa88ea53a52a1
Abigail-Diaz <123652655+Abigail-Diaz@users.noreply.github.com>
2023-04-18 19:17:06 -0700

    Added platform collision dynamics

--- main.py
+++ main.py
@@ -23,24 +23,27 @@ clock = pg.time.Clock()
 space_background = pg.image.load('media/background3Medium.jpeg').convert()
 cat = pg.image.load('media/cat.png').convert_alpha()
 bat = pg.image.load('media/bat-x1.gif').convert_alpha()
+platform = pg.image.load('media/ground.png').convert_alpha()
 
 # create the rectangles of each character
-cat_character = pg.transform.smoothscale(cat, (80, 80))
+cat = pg.transform.smoothscale(cat, (80, 80))
+platform = pg.transform.smoothscale(platform, (90, 50))
 bat = pg.transform.smoothscale(bat, (80, 80))
-cat_rect = cat_character.get_rect(midbottom = (0, floor + 30))
+cat_rect = cat.get_rect(midbottom = (0, floor + 30))
 bat_rect = bat.get_rect(midbottom = (screen_width, floor - 10))
+platform_rect = platform.get_rect(midbottom= (screen_width/2, screen_length/2))
 
 # Variables for cat movements
 player_gravity = 0
 cat_jump = False
-
-# Background movement
-background_motion = 0
+cat_fall = False
 
 # Cat movement
 move_right = False
 move_left = False
 
+# Cat Actions
+recent_platform_collide = False
 
 # Assume game is running
 running = True
@@ -78,6 +81,8 @@ while running:
     screen.blit(space_background, (100, 0))
     screen.blit(space_background, (550 , 0))
     
+    #Display Platform
+    screen.blit(platform, platform_rect)
     
     # ~~~~ Cat Animation ~~~~
     # Cat movement
@@ -87,12 +92,31 @@ while running:
     if move_left == True:
         cat_rect.x -= 2
     
-    # Gravity: Cat will fall down as y values increase
-    player_gravity += 1
-    cat_rect.y += player_gravity
+    # Collision with platform
+    collide_platform = pg.Rect.colliderect(cat_rect, platform_rect)
     
+    # Keep cat on top of platform
+    if collide_platform:
+        cat_jump = False
+        cat_rect.bottom = platform_rect.top
+        recent_platform_collide = True
+    
+    # Cat falls when outside platform
+    if cat_rect.x > platform_rect.x and recent_platform_collide == True:
+        cat_fall = True
+        recent_platform_collide = False
+    
+    # ~~ Jump Dynamics ~~
+    # Gravity: Cat will fall down as y values increase
+    if cat_fall == True:
+        player_gravity += 1
+        cat_rect.y += player_gravity
+        
     # cat will move on the x axis as it completes the jump
     if cat_jump == True:
+        cat_fall = False
+        player_gravity += 1
+        cat_rect.y += player_gravity
         cat_rect.x += 3
     
     # check if cat reached the floor
@@ -105,8 +129,8 @@ while running:
     # check if the cat goes out of the screen's range
     if cat_rect.left > screen_width:  
         cat_rect.x = 0
-        
-    screen.blit(cat_character, cat_rect)
+    
+    screen.blit(cat, cat_rect)
     
     # Bat movements: Moves from left to right
     bat_rect.right -= 1
@@ -115,9 +139,6 @@ while running:
     # check if the bat goes out of the screen's range    
     if bat_rect.left < 0:
         bat_rect.left = screen_width
-    
-    #if background_motion == 100:
-        #background_motion  = -100
     
     #update screen
     pg.display.flip()
