f1dbba3a33e25373c5adb5f60ea4bf2ba7591588
Abigail-Diaz <123652655+Abigail-Diaz@users.noreply.github.com>
2023-05-11 20:01:35 -0700

    update on levels class

--- Python Files/Levels.py
+++ Python Files/Levels.py
@@ -6,8 +6,10 @@ level_map = [
 'xx  xxx       xx           ',
 '                           ',
 'xxxx                   xx  ',
-'xxxx       x               ',
+'xxxx   E    x      E       ',
 'xx  x xxxx    xx  xx       ',
 '    x xxxx    xx  xxx      ',
 '      xxxxxx  xx  xxxx     ',
 'x  xxxxxxxxx  xx  xxxx     ']
+
+
