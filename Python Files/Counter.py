4c3f1f9c1719ffe0d13cb318ecf93dde2be06fd0
Abigail-Diaz <123652655+Abigail-Diaz@users.noreply.github.com>
2023-05-22 17:44:03 -0700

    updated health bar on counter.py and added a check for player falling off a platform

--- Python Files/Counter.py
+++ Python Files/Counter.py
@@ -1,32 +1,86 @@
 
 import pygame as pg
+from Menu import Menu
 
-class Counter:
-    def __init__(self, screen):
-        self.image = pg.image.load('media/bar.png').convert_alpha()
-        
-        self.heigth = 50
-        self.length = 100
-        #self.red_block = pg.display.set_mode((400,100))
-        #self.red_block.fill('red')
-        #self.red_block = pg.transform.smoothscale(self.red_block, (self.length, self.heigth))
-        self.red_block = pg.Surface((self.length, self.heigth))
+
+class Counter():
+    def __init__(self, screen, player, level):
+        self.flag_width = 50
+        self.flag_height = 50
+        self.posx = 500
+        self.posy = 409
+        self.health_bar_posx = screen.get_width() - 150
+        self.health_bar_posy = screen.get_height() - (screen.get_height() - 10)
+        self.set_image_paths_dict()
+        self.health_bar_height = 20
+        self.initial_health_bar_length = 100
+        self.health_bar_length = self.initial_health_bar_length
+        self.original_image = pg.image.load('media/bar.png').convert_alpha()
+        self.original_image = pg.transform.smoothscale(self.original_image, (self.health_bar_length, self.health_bar_height))
+        self.red_block = pg.Surface((self.health_bar_length, self.health_bar_height))
         self.red_block.fill('red')
-        
-        self.image = pg.transform.smoothscale(self.image, (self.length, self.heigth))
+        self.lost = False
+        self.won = False
+        self.player = player
+        self.green_bar_image = self.original_image
         self.screen = screen
+        self.menu = Menu(screen)
+        self.level = level
+        self.flag_rect = pg.Rect(self.posx, self.posy, self.flag_width, self.flag_height)
     
     def run(self):
-        
-        self.screen.blit(self.red_block, (400, 100))
-        self.screen.blit(self.image, (400, 100))
+        self.check_lost() 
+        self.screen.blit(self.red_block, (self.health_bar_posx, self.health_bar_posy))
+        self.screen.blit(self.green_bar_image, (self.health_bar_posx, self.health_bar_posy))
         
     def collision(self):
         self.update_size()
 
-    def update_size(self):
-        self.length -= 10
-        if self.length < 0:
+    def update_size(self): 
+        '''Reduce the length of the green health bar'''
+        
+        # reduce health bar length
+        self.health_bar_length -= 10
+
+        # check if health bar has run out of length
+        if self.health_bar_length < 0:
             print('dead')
+            self.lost = True
+            self.player.physics.fall = False
+            
         else:
-            self.image = pg.transform.smoothscale(self.image, (self.length, self.heigth))
+            self.green_bar_image = pg.transform.smoothscale(self.original_image, (self.health_bar_length, self.health_bar_height))
+
+    
+    def check_lost(self):
+        '''Check if player has either fallen off the screen or if lost'''
+        
+        # check if player has fallen off a cliff, update to 'lost' status if true
+        if self.player.rect.y > 1000:
+            self.lost = True
+            self.player.physics.fall = False
+
+            self.level.restart()
+            self.player.restart()
+        
+        # check if player has lost 
+        if self.lost:
+            self.menu.restart_menu()
+    
+
+    def restart(self):
+        self.lost = False
+        self.health_bar_length = self.initial_health_bar_length
+        self.green_bar_image = pg.transform.smoothscale(self.original_image, (self.health_bar_length, self.health_bar_height))
+        
+
+    def get_image_paths_array(self, path):
+        self.image_file_paths = []
+        for item in range(5):
+            self.image_file_paths.append(f'{path}{item + 1}.png')
+        return self.image_file_paths
+    
+    def set_image_paths_dict(self):
+        self.image_paths_dict = {'idle': None} 
+        self.image_paths_dict['idle'] = self.get_image_paths_array(f'media/flag_')
+        
