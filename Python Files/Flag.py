7dc7c93886b0c196e77af14b5f681b03733c9b2b
Abigail-Diaz <123652655+Abigail-Diaz@users.noreply.github.com>
2023-05-22 17:58:36 -0700

    created a flag class

---
+++ Python Files/Flag.py
@@ -0,0 +1,15 @@
+
+from Sprites import Sprite
+import pygame as pg
+
+class Flag(Sprite):
+    def __init__(self, posx, posy, image_width, image_length):
+        self.flag_width = 50
+        self.flag_height = 50
+        self.rect = pg.Rect(posx, posy, image_width, image_length)
+        self.action_array = ['flag']
+        super().__init__(self.rect, image_width, image_length, self.action_array)
+    
+    def update(self, update = 0):
+        self.update_sprite()
+        self.rect.x += update
