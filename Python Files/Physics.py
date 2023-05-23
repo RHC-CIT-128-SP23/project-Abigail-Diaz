1ead3bdf6fe3d597cd31221ef1fd5adb74e8f0eb
Abigail-Diaz <123652655+Abigail-Diaz@users.noreply.github.com>
2023-05-22 17:39:48 -0700

    added flag collision detection on physics.py and cleaned up main.py

--- Python Files/Physics.py
+++ Python Files/Physics.py
@@ -1,10 +1,12 @@
 
-import copy
 import pygame as pg
 
 class Physics:
-    def __init__(self, rect):
-        self.rect = rect
+    '''Manages player's movements and checks for collisions'''
+    def __init__(self, player, level):
+        self.player = player
+        self.rect = player.rect
+        self.level = level
         self.jump = False
         self.fall = True
         self.gravity = 0
@@ -14,38 +16,44 @@ class Physics:
         self.velocity = 0
         self.direction = {"right": False, "left": False, 'down': True, 'up': False}
         self.current_direction = None
+        self.on_floor_counter = 0
+        self.non_col = 0
     
-    def move(self, level): 
-        self.movement()
-        self.check_if_jumping(level)
+    def run(self):
+        '''check player's actions'''
+        self.horizontal_movement()
+        self.jumping()
         self.falling()
     
-    def falling(self):
-        # Character will fall down as y values increase
-        if self.fall == True:
-            self.direction['up'] = False
-            self.gravity += 0.5  
-            self.rect.y += self.gravity # gravity is never set back to zero
-    
-    def check_if_jumping(self, level):
+    def horizontal_movement(self):
+        '''Moves player left or right'''
+        if self.move_right and self.player.rect.x < 300:
+            self.rect.x += 3
+            
+        if self.move_left:
+            self.rect.x -= 1
+
+    def jumping(self):
         # Character will move on the x axis as it completes the jump
-        if self.jump == True:
+        if self.jump:
             self.fall = True 
-            self.direction['down'] = True
+
             if self.move_left:
                 self.rect.x -= 5
                 
             elif self.move_right:
-                self.rect.x += 1
-                level.move_screen_forward(-2)
-                
-                
+                self.level.move_screen_forward(-2)
+
+    def falling(self):
+        # Character will fall down as y value of rect increase
+        if self.fall:
+            self.gravity += 1
+            self.rect.y += self.gravity 
     
     def make_jump(self):
         self.jump = True
-        self.direction['up'] = True
-        self.gravity = -10
-    
+        self.gravity = -15
+
     def stop_jump(self):
         self.jump = False
     
@@ -55,58 +63,103 @@ class Physics:
     def stop_fall(self):
         self.fall = False
     
-    def stop(self):
-        for key in self.direction:
-            self.direction[key] = False
-
-    def movement(self):
-        if self.move_right:
-            self.rect.x += 2 # delete
-            
-        if self.move_left == True:
-            self.rect.x -= 2
-            
-
+    
     def floor_reached(self, floor):
-        '''check if cat reached the floor # Condition: If cat reached the floor, then it must be false that he is jumping'''
+        '''Keep player on the floor'''
+
+            # prevent player from falling through the tiles
         if self.rect.bottom >= floor: 
             self.rect.bottom = floor
-            self.jump = False
+            self.jump = False                                                                                                  
     
-    def range_reached(self, screen_width, screen_lenght, new_pos):
-        '''Reset cat position the the start (left) of the screen if it goes out of the screen's range'''
+    def range_reached(self):
+        '''Reset cat position to the the start (left) of the screen if it goes out of the screen's range'''
         if self.rect.left < 0:  
             self.rect.x = 25
     
-    def set_position(self, x, y):
-        self.rect.x = x
-        self.rect.y = y
-    
     def repel_to_left(self):
-        '''Repel character to the left by decreasing x value '''
-        print('left repelled')
-        self.rect.x -= 2
+        '''Repel player to the left by decreasing x value '''
+        self.rect.x -= 5
 
     def repel_to_right(self):
-        '''Repel character to the righ by increasing x value'''
-        print('right repelled')
+        '''Repel player to the right by increasing x value'''
         self.rect.x += 5
 
-    
     def repel_down(self):
-        '''Repel character down by incresing its y value'''
+        '''Repel player down by increasing its y value'''
         print('down repelled')
         self.rect.y += 10
         self.fall = True
 
-    def collision_data(self, collision_rect):
-        # left side of cat in between the rect
-        self.left_in_between = self.rect.left > collision_rect.left and self.rect.left < collision_rect.right
+    
+    def check_flag_collision(self):
+        if self.player.visible:
+            self.flag_collision = pg.sprite.spritecollide(self.player, self.level.flags, False, pg.sprite.collide_mask)
+        
+        if self.flag_collision:
+            self.player.counter.menu.won_menu()
+            self.player.won()
+            
 
-        # right side of cat in between the rect
-        self.right_in_between = self.rect.right > collision_rect.left and self.rect.right < collision_rect.right
+    def collision_check(self):
 
-        self.on_floor = (self.rect.bottom < collision_rect.bottom) and (self.left_in_between or self.right_in_between)
+        left_collision = False
 
-        if self.on_floor:
-            print('on floor: ', self.on_floor)
+        '''Collision check with flag'''
+        self.check_flag_collision()
+        
+        '''Collision Checks with enemy'''
+
+        enemy_collision = pg.sprite.spritecollide(self.player, self.level.enemies, False, pg.sprite.collide_mask)
+        enemy_rect_collision = self.level.get_enemy_collision_rect(self.player)
+        if enemy_collision:
+            if self.rect.x < enemy_rect_collision.x:
+                self.player.update_current_sprite('damage')
+                self.rect.x -= 20
+            if self.rect.x > enemy_rect_collision.x: # replace with else?
+                self.player.update_current_sprite('damage')
+                self.rect.x += 20
+            self.player.counter.collision()
+
+        collision = pg.sprite.spritecollide(self.player, self.level.tiles, False, pg.sprite.collide_mask)
+
+        if self.direction['right'] and self.player.rect.x >= 300 and left_collision == False:# and cat.rect.x > 200
+            self.level.move_screen_forward(-2) 
+
+        if collision:
+            self.gravity = 0
+            self.non_col = 0
+            self.fall = False
+            self.jump = False                                                                                             
+            
+            collision_rect = self.level.get_collision_rect(self.player)
+    
+            # Upon collision, check if cat is on top of a rectangle
+            if  ((self.player.rect.left < collision_rect.right or self.player.rect.right > collision_rect.left) and ((abs(self.rect.bottom - collision_rect.top) < 50))):
+                self.fall = False
+                self.on_floor_counter += 1
+            
+                # Collision with the floor
+                if (abs(self.rect.bottom - collision_rect.top)) < 50:
+                    self.rect.bottom = collision_rect.top + 0.5
+
+            elif  ((self.rect.left < collision_rect.right or self.rect.right > collision_rect.left) and (abs(self.rect.top - collision_rect.bottom) < 20)):
+                self.repel_down()
+        
+            elif collision_rect.right > self.rect.right and (self.rect.bottom > collision_rect.top):
+                self.fall = False
+                self.on_floor_counter = 0
+                self.repel_to_left()
+                left_collision = True
+        
+            elif collision_rect.left < self.rect.left and (self.rect.bottom > collision_rect.top):
+                self.repel_to_right()
+                self.on_floor_counter = 0
+                self.fall = False
+
+        else:
+            # prevents non-intended fall from becoming true when else becomes true for a moment
+            # to avoid alternation between else and floor collision, so fall does not become true while being on the floor
+            if self.non_col > 4: 
+                self.fall = True
+            self.non_col += 1
--- Python Files/main.py
+++ Python Files/main.py
@@ -7,221 +7,96 @@
 
 import pygame as pg
 import sys
-import math
-from Counter import Counter
-from Enemy import Enemy
 from Player import Player
 from Platform import Level, screen_width, screen_length
-from Physics import Physics
-from Sprites import Sprite
-import random
-from Fire import Fire
 from Shoot import Shoot
 from Levels import *
 from Sound import Sound
+from Menu import Menu
 
+def main():
 
-# start pygame
-pg.init()
-floor = 420
-fps = 60
-non_col = 0
-on_floor_counter = 0
-press_x_key_count = 0
+    # start pygame
+    pg.init()
 
+    fps = 60
 
-# Set window display
-screen = pg.display.set_mode((screen_width, screen_length))
-pg.display.set_caption('Space Cat')
-clock = pg.time.Clock()
-sound = Sound()
-
-
-# Store file paths for each sprite image 
-cat_walk_sprites_file_paths = []
-cat_idle_sprites_file_paths = []
-cat_damage_sprites_file_paths = []
-
-eye_image_file_paths = []
-platform_image_file_paths = []
-
-# Will store all sprite file paths arrays according to their intended action
-cat_sprites_file_paths = {'walk': None, 'idle': None, 'damage': None}     
-
-# Save the file path names in their intended array
-for item in range(6):
-    cat_walk_sprites_file_paths.append(f"media/walk_{item + 1}.png")
-
-for item in range(4):
-    cat_idle_sprites_file_paths.append(f"media/idle_{item + 1}.png")
-
-for item in range(5):
-    cat_damage_sprites_file_paths.append(f"media/damage_{item + 1}.png")
-
-on_floor = False # delete
-
-for item in range(3):
-    eye_image_file_paths.append(f'media/eye_{item + 1}.png')
-
-# Assign the file path arrays to their corresponding place in the dict
-cat_sprites_file_paths['walk'] = cat_walk_sprites_file_paths
-cat_sprites_file_paths['idle'] = cat_idle_sprites_file_paths
-cat_sprites_file_paths['damage'] = cat_damage_sprites_file_paths
-
-# create objects 
-cat = Player(120, 281,  60, 40, cat_sprites_file_paths)
-cat_physics = Physics(cat.rect)
-
-# Holds current level map
-level = Level(screen)
-counter = Counter(screen)
-shoot = Shoot(90, 90, cat, level.enemies, screen)
-sound.play_background_music()
-
-running = True
-
-while running:
-    for event in pg.event.get():
-        if event.type ==  pg.QUIT:
-            pg.quit()
-            sys.exit()
+    # Set window display
+    screen = pg.display.set_mode((screen_width, screen_length))
+    pg.display.set_caption('Space Cat')
+    clock = pg.time.Clock()
     
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_SPACE:
-                cat_physics.make_jump()
-                touching_floor = False
+    # game feautures objects
+    sound = Sound()
+    menu = Menu(screen)
+    level = Level(screen)
 
+    cat = Player(120, 281,  60, 40, level, screen)
+
+    shoot = Shoot(90, 90, cat, level.enemies, screen)
+
+    running = True
+
+    while running:
+        sound.play_background_music()
+    
+        for event in pg.event.get():
+            if event.type ==  pg.QUIT:
+                pg.quit()
+                sys.exit()
+    
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    cat.physics.make_jump()
         
-        if event.type ==  pg.KEYDOWN:
-            if event.key == pg.K_RIGHT:
-                cat.update_current_sprite('walk')
-                cat.update_spr = True
-                cat_physics.move_right = True
-                cat_physics.direction['left'] = False
-                cat_physics.direction["right"] = True
-                cat_physics.move_left = False
+            if event.type ==  pg.KEYDOWN:
+                if event.key == pg.K_RIGHT:
+                    cat.update_current_sprite('walk')
+                    cat.update_spr = True
+                    cat.physics.move_right = True
+                    cat.physics.direction['left'] = False
+                    cat.physics.direction["right"] = True
+                    cat.physics.move_left = False
     
-            if event.key == pg.K_LEFT:
-                cat.update_current_sprite('walk')
-                cat_physics.move_left = True
-                cat_physics.move_right= False
-                cat_physics.direction["right"] = False
-                cat_physics.direction['left'] = True
-                cat.update_spr = True
+                if event.key == pg.K_LEFT:
+                    cat.update_current_sprite('walk')
+                    cat.physics.move_left = True
+                    cat.physics.move_right= False
+                    cat.physics.direction["right"] = False
+                    cat.physics.direction['left'] = True
+                    cat.update_spr = True
 
-            if event.key == pg.K_x:
-                shoot.running = True # prevents fire sprite from appearing before 'x' is pressed
-                
-                press_x_key_count += 1
-                print('press_x_key_count: ', press_x_key_count)
-                if press_x_key_count > 1:
+                if event.key == pg.K_x:
+                    shoot.running = True # prevents fire sprite from appearing before 'x' is pressed
                     shoot.attack()
-                    if press_x_key_count > 2:
-                        press_x_key_count = 0
-                else:
-                    shoot.shooting = False
 
-        if event.type == pg.KEYUP:
-            if event.key == pg.K_RIGHT or event.key == pg.K_LEFT:
-                cat.update_current_sprite('idle')
-                cat.update_spr = True
-                cat_physics.direction["right"] = False
-                cat_physics.move_left = False
-                cat_physics.move_right = False
-                cat_physics.direction["left"] = False
-            if event.key == pg.K_x:
-                pass
-                #shoot.shooting = False
-
-    level.run()
-    counter.run()
-    #sound.play_background_music()
-
-    # Activate the fire sprite group display and sprite update
-    shoot.run()
-    
-    ''' Cat Actions'''
-    # Activate physics function checks
-    cat_physics.move(level)
-    
-    # check if the cat goes out of the screen's range
-    cat_physics.range_reached(screen_width, screen_length, 0)
-
-    # Display Cat Character
-    cat.update_sprite() 
-    
-    '''Collision Checks'''
-
-    enemy_collision = pg.sprite.spritecollide(cat, level.enemies, False, pg.sprite.collide_mask)
-    enemy_rect_collision = level.get_enemy_collision_rect(cat)
-    if enemy_collision:
-        if cat.rect.x < enemy_rect_collision.x:
-            cat.update_current_sprite('damage')
-            cat.rect.x -= 70
-        if cat.rect.x > enemy_rect_collision.x:
-            cat.update_current_sprite('damage')
-            cat.rect.x += 70
-        counter.collision()
-
-    
-
-    collision = pg.sprite.spritecollide(cat, level.tiles, False, pg.sprite.collide_mask)
-    
-    if collision:
-        non_col = 0
-        cat_physics.fall = False
-        cat_physics.jump = False
-        cat_col = cat.rect.bottom
+            if event.type == pg.KEYUP:
+                if event.key == pg.K_RIGHT or event.key == pg.K_LEFT:
+                    cat.update_current_sprite('idle')
+                    cat.update_spr = True
+                    cat.physics.direction["right"] = False
+                    cat.physics.move_left = False
+                    cat.physics.move_right = False
+                    cat.physics.direction["left"] = False
         
-        print('1: IN collision')
-        collision_rect = level.get_collision_rect(cat)
-        rect_col = collision_rect.top
-    
-            # Upon collision, check if cat is on top of a rectangle
-        if  ((cat.rect.left < collision_rect.right or cat.rect.right > collision_rect.left) and ((abs(cat.rect.bottom - collision_rect.top) < 50))):
-            cat_physics.fall = False
-            on_floor_counter += 1
-            
-            # Collision with the floor
-            if (abs(cat.rect.bottom - collision_rect.top)) < 50:
-                cat.rect.bottom = collision_rect.top + 0.5
-            
-            if cat_physics.direction['right']:# and cat.rect.x > 200
-                level.move_screen_forward(-2) 
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1: 
+                    mouse_click_pos = pg.mouse.get_pos()
+                    if menu.start_text_rect.collidepoint(mouse_click_pos):
+                        cat.counter.menu.start = False
+                    if cat.counter.menu.restart_rect.collidepoint(mouse_click_pos):
+                        
+                        cat.restart()
+                        cat.counter.restart()
+                        level.restart()
+        level.run()
+        cat.counter.run() 
+        cat.run()
+        shoot.run()
 
-            print('x pos: ', cat.rect.x)
-            print('y pos: ', cat.rect.y)
+        cat.counter.menu.start_menu() 
+        pg.display.flip()
+        clock.tick(fps)
 
-        elif  ((cat.rect.left < collision_rect.right or cat.rect.right > collision_rect.left) and (abs(cat.rect.top - collision_rect.bottom) < 20)):
-            print('bottom collision')
-            cat_physics.repel_down()
-        
-        elif collision_rect.right > cat.rect.right and (cat.rect.bottom > collision_rect.top):
-            cat_physics.fall = False
-            on_floor_counter = 0
-            cat_physics.repel_to_left()
-        
-        elif collision_rect.left < cat.rect.left and (cat.rect.bottom > collision_rect.top):
-            cat_physics.repel_to_right()
-            on_floor_counter = 0
-            cat_physics.fall = False
-
-    else:
-            # prevents non-intended fall from becoming true when else becomes true for a moment
-            # to avoid alternation between else and floor collision, so fall does not become true while being on the floor
-        if non_col > 4: 
-            cat_physics.fall = True
-            print('greater than 3')
-    
-        print('else')
-        cat_physics.direction['down'] = True
-        non_col += 1
-            
-
-    '''Enemy Eye Actions'''
-
-    #update screen
-    screen.blit(cat.image, cat.rect) 
-    
-    pg.display.flip()
-    clock.tick(fps)
+if __name__ == "__main__":
+    main()
